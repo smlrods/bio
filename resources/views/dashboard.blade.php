20860e0fe5f3040c00e5e72b8bc7ad163f8586f3
smlrods <smlrodrigues.dev@gmail.com>
2024-04-13 14:21:44 -0300

    Update dashboard.blade.php to include links for images, testimonials, and links

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,12 +5,32 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12 flex flex-col gap-2">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 w-full">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+                <a href="./product-photos">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Imagens") }}
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 w-full">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <a href="./testimonials">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Feedbacks") }}
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 w-full">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <a href="./links">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Links") }}
+                    </div>
+                </a>
             </div>
         </div>
     </div>
