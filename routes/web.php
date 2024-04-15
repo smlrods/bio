867aea12dae0ada727f734ac35aa4872778abd64
smlrods <84884629+smlrods@users.noreply.github.com>
2024-04-15 19:33:30 -0300

    Add `product-photos` resource route to `web.php`

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductPhotoController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/product-photos', ProductPhotoController::class);
 });
 
 require __DIR__.'/auth.php';
