663d1a30020b3a6032d44645ba527132f41befd8
smlrods <84884629+smlrods@users.noreply.github.com>
2024-04-16 21:17:15 -0300

    Add pageInfo relationship to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -59,5 +60,10 @@ class User extends Authenticatable
     public function links(): HasMany
     {
         return $this->hasMany(Link::class);
+    }
+
+    public function pageInfo(): HasOne
+    {
+        return $this->hasOne(PageInfo::class);
     }
 }
