5f86abdb24583d2c313107c58da0f889991a2ef8
smlrods <84884629+smlrods@users.noreply.github.com>
2024-04-16 21:17:48 -0300

    Add PageInfo Controller

---
+++ app/Http/Requests/PageInfoRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PageInfoRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string|max:255',
+            'logo' => 'sometimes|image'
+        ];
+    }
+}
