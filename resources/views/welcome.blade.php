654ccb840d800cbee474a076d5501e5bb75445f7
smlrods <84884629+smlrods@users.noreply.github.com>
2024-04-23 17:11:57 -0300

    Update font to Playfair Display Font on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bio</title>
     @vite(['resources/css/app.css'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body class="font-sans antialiased min-h-screen min-w-full flex justify-center items-center">
-    <div class="w-1/2 text-center">
-        <h1 class="text-5xl font-bold mb-5">Your content, curated. Let us showcase your best work, creations, and
+    <div class="sm:w-1/2 mx-2 text-center">
+        <h1 class="font-playfair sm:text-5xl font-bold mb-5 text-2xl">Your content, curated. Let us showcase your best work, creations, and
             everything
             you want to share.</h1>
-        <div>
+        <div class="font-playfair font-bold">
             @auth
                 <a href="/dashboard" class="border border-black px-5 py-2 hover:bg-black hover:text-white">Dashboard</a>
             @else
